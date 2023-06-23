6407d0c15fc232fd8237562dba190596f132aac2
Ahmed Raza <ahmedraza6100@gmail.com>
2023-06-23 21:12:19 +0500

    refactored code

--- functions.mjs
+++ functions.mjs
@@ -1,32 +1,30 @@
 function handlesubmit(event) {
   event.preventDefault();
   let data = document.querySelector("#input");
-  document.querySelector("#celcius").innerHTML = data.value;
-  let img = document.querySelector(".weatherIcon img").src;
-  console.log('first', img)
-  
+  let img = document.querySelector(".weatherIcon img");
+  let status = document.querySelector(".weatherCondition");
+  let temp = document.querySelector("#celcius");
+
+  temp.innerHTML = data.value;
   let weather = data.value;
+
   if (weather.length == 0) {
-    document.querySelector("#celcius").innerHTML = "0";
-    document.querySelector(".weatherCondition").innerHTML = "Enter Temp";
+    temp.innerHTML = "0";
+    status.innerHTML = "Enter Temp";
     console.log("hot weather");
-  } else if (weather > 100) {
-    document.querySelector("#celcius").innerHTML = "0";
-    document.querySelector(".weatherCondition").innerHTML = "Enter Valid Temp";
-    document.querySelector(".weatherIcon img").src = './images/959873.png';
-  } else if (weather < -100) {
-    document.querySelector("#celcius").innerHTML = "0";
-    document.querySelector(".weatherCondition").innerHTML = "Enter Valid Temp";
-    document.querySelector(".weatherIcon img").src = './images/959873.png';
+  } else if (weather > 100 || weather < -100) {
+    temp.innerHTML = "0";
+    status.innerHTML = "Enter Valid Temp";
+    img.src = "./images/959873.png";
   } else if (weather > 30) {
-    document.querySelector(".weatherCondition").innerHTML = "Too hot";
-    document.querySelector(".weatherIcon img").src = './images/day.svg';
+    status.innerHTML = "Too hot";
+    img.src = "./images/day.svg";
   } else if (weather > 20) {
-    document.querySelector(".weatherCondition").innerHTML = "Moderate";
-    document.querySelector(".weatherIcon img").src = './images/cloudy-day-1.svg';
-  } else if (weather > 0) {
-    document.querySelector(".weatherCondition").innerHTML = "Too cold";
-    document.querySelector(".weatherIcon img").src = './images/snowy-6.svg';
+    status.innerHTML = "Moderate";
+    img.src = "./images/cloudy-day-1.svg";
+  } else if (weather > -100) {
+    status.innerHTML = "Too cold";
+    img.src = "./images/snowy-6.svg";
   }
 }
 
